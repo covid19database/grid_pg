9603191756d9613e13887dfa887106a87deb86f0
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-04-16 17:08:44 -0700

    cleaning up old sql stuff

--- grid-db/grid-sql/setup.sql
+++ grid-db/grid-sql/setup.sql
@@ -1,22 +1,19 @@
-CREATE TABLE IF NOT EXISTS grid(
-    time TIMESTAMP NOT NULL,
-    pluscode TEXT NOT NULL,
-    attributes HSTORE
+CREATE TABLE IF NOT EXISTS attributes(
+    grid_time   TIMESTAMP NOT NULL,
+    grid_loc    TEXT      NOT NULL,
+    attribute   TEXT      NOT NULL,
+    counter     INTEGER   NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX idx_grid_attrs ON grid USING GIN(attributes);
-CREATE UNIQUE INDEX IF NOT EXISTS grid_time_code_idx ON grid(time, pluscode);
-
-CREATE TABLE IF NOT EXISTS update_log(
-    time TIMESTAMP NOT NULL,
-    pluscode TEXT NOT NULL,
-    nonce   BYTEA NOT NULL, -- sha256 hash
-    attributes HSTORE
-);
-CREATE UNIQUE INDEX IF NOT EXISTS update_log_uniq_nonce ON update_log(nonce);
-
-SELECT create_hypertable('grid', 'time');
-
+CREATE OR REPLACE VIEW grid AS
+    SELECT grid_time as time,
+           grid_loc as location,
+           attribute,
+           sum(counter) as count
+    FROM attributes
+    GROUP BY time, location, attribute
+    ORDER BY time ASC;
 
 CREATE OR REPLACE FUNCTION plus_code(poly TEXT) RETURNS TEXT AS $$
 import sys
@@ -44,46 +41,4 @@ CREATE OR REPLACE FUNCTION half_hour(ts TIMESTAMP) RETURNS TIMESTAMP AS $$
 from datetime import datetime, timedelta
 tss = datetime.strptime(ts, '%Y-%m-%d %H:%M:%S')
 return tss - (tss - datetime.min) % timedelta(minutes=30)
-$$ LANGUAGE plpython3u;
-
-
-CREATE FUNCTION update_grid() 
-RETURNS INTEGER 
-TRANSFORM FOR TYPE hstore
-AS $$
-res = plpy.execute("SELECT half_hour(time) as hh, pluscode, attributes FROM update_log ORDER BY hh, pluscode")
-if len(res) == 0:
-    return 0
-first = res[0]
-key = (first["hh"], first["pluscode"])
-attrs = first["attributes"]
-num = 0
-
-def merge(key, original, new):
-    if key == "feels_sick":
-        oval = original == 'True'
-        nval = new == 'True'
-        return str(nval or oval)
-        
-for row in res[1:]:
-    pass
-    newkey = (row["hh"], row["pluscode"])
-    if newkey == key:
-        for k,v in row["attributes"].items():
-            if k in attrs:
-                attrs[k] = merge(k, attrs[k], v)
-            else:
-                attrs[k] = v
-    else:
-        stmt = plpy.prepare("INSERT INTO grid(time, pluscode, attributes) VALUES\
-                            ($1, $2, $3) ON CONFLICT DO NOTHING", ["timestamp", "varchar", "hstore"])
-        plpy.execute(stmt, [key[0], key[1], attrs])
-        num += 1
-        attrs = row["attributes"]
-        key = (row["hh"], row["pluscode"])
-stmt = plpy.prepare("INSERT INTO grid(time, pluscode, attributes) VALUES\
-                    ($1, $2, $3) ON CONFLICT DO NOTHING", ["timestamp", "varchar", "hstore"])
-plpy.execute(stmt, [key[0], key[1], attrs])
-num += 1
-return num
 $$ LANGUAGE plpython3u;
